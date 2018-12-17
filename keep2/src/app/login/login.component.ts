ef393662c1a9e889a3bc3b413744370da472b8f8
prasannambe <45001654+prasannambe@users.noreply.github.com>
2018-12-17 16:33:46 +0530

    Add files via upload

--- keep2/src/app/login/login.component.ts
+++ keep2/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { AuthenticationService } from '../services/authentication.service';
+import { RouterService } from '../services/router.service';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +9,23 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-    username = new FormControl();
-    password = new FormControl();
+    username = new FormControl('',[ Validators.required ]);
+    password = new FormControl('',[ Validators.required ]);
+    public bearerToken : any;
+    constructor(private authservice: AuthenticationService,
+                private routerService : RouterService){
 
+    }
     loginSubmit() {
-      console.log('entered');
+      this.authservice.authenticateUser({"username" : this.username.value,"password" : this.password.value})
+      .subscribe( res => {
+        this.bearerToken = res['token'];
+        this.authservice.setBearerToken(this.bearerToken);
+        this.routerService.routeToDashboard();
+      },
+      err => {
+
+      })
     }
     get_username_error(){
       return this.username.hasError('required')?'username is required':'';
